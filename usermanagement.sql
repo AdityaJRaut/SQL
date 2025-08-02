ff8c127ef184cd2e3d581da8ffbeefb49b941c5a
Aditya Raut <rautadityaj@gmail.com>
2025-08-02 09:37:28 +0530

    new

---
+++ usermanagement.sql
@@ -0,0 +1,5 @@
+
+insert into employee (empname,empdept,empgender,empdob,managerid,user_id,contact,email)
+values('admin','admin','male','1994-01-28','1','1',9876543210,'admin@gmail.com');
+
+insert into users (username,password,role,status) values ('admin','admin','admin','active');
